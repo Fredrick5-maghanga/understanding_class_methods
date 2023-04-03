df2a9e4a2442080a6aa32f6c5b51932526c5b3c5
Fredrick5-maghanga <maghangafredrick5@gmail.com>
2023-04-03 16:57:55 +0300

    Add project files.

---
+++ classmethods/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classmethods
+{
+   public class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        //constructor
+        public Employee(string first, string last, string em, DateTime bd) :this(first,last,em,bd, 0)  
+        {
+            
+
+        }
+
+        public Employee( string first, string last, string em, DateTime bd, double rate) 
+        {
+            firstName= first;
+            lastName= last;
+            email= em;
+            birthDay= bd;
+            hourlyRate= rate;
+           
+            
+        }
+
+        public void performWork()
+        {
+            //  numberOfHoursWorked++;
+            performWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+        public void performWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for  {numberOfHours} hour(s)!");
+            
+        }
+        public double ReceiveWage(bool resetHours = true) 
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+            Console.WriteLine($" {firstName} {lastName} has recived a wage of {wage} for {numberOfHoursWorked } hour(s) of work.");
+
+            if ( resetHours ) 
+                numberOfHoursWorked = 0;
+
+            return wage;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($" \nFirstName: \t{firstName} \n{lastName}: \t {lastName}\nEmail: \t\t{email} \nBirthday: \t{birthDay.ToShortDateString()}\n");
+        }
+    }
+}
---
+++ classmethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using classmethods;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("----------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime (1997, 06, 23), 25 ); 
+
+bethany.DisplayEmployeeDetails();
+
+bethany.performWork();
+bethany.performWork();
+bethany.performWork(5);
+bethany.performWork();
+
+double receivedWageBethany = bethany.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from program): {receivedWageBethany}");
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("----------\n");
+
+Employee george = new Employee("george", "Smith", "george@snowball.be", new DateTime(1997, 06, 23), 25);
+
+george.DisplayEmployeeDetails();
+
+george.performWork();
+george.performWork();
+george.performWork(5);
+george.performWork();
+george.performWork(8);
+double receivedWagegeorge = george.ReceiveWage(true);
+Console.WriteLine($"Wage paid (message from program): {receivedWagegeorge}");
+
+
